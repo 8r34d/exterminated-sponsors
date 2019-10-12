88d4ba8e9c0b93e305365542990a7a9ba52347b8
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-10-12 23:23:36 +0100

    edit index route and view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Exterminated Sponsors'
+    h1: 'Exterminated Sponsors',
+    title: 'Express Demonstration'
   });
 });
 
