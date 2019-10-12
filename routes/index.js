522ce0e8eec476034ca61c4d809b5f336911e7d8
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-10-12 23:11:33 +0100

    add generated app

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Exterminated Sponsors'
+  });
+});
+
+module.exports = router;
