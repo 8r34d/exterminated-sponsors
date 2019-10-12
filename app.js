d53e4b1e82d55c493dc94dd317321026a356b502
Dean Spencer <dean_spencer@yahoo.co.uk>
2019-10-12 22:43:17 +0100

    add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
